dc3cb2ad5fa767966514b8a583cb0c34ce1da300
Irina Ialticenco <irina.braghina@gmail.com>
2021-08-28 22:27:46 +0300

    Implement task4

---
+++ Homework1/src/Task4.java
@@ -0,0 +1,15 @@
+public class Task4 {
+    public static void main(String[]args){
+        int c,f;
+        float r;
+        String s;
+        c = 8;
+        f = 8;
+        r = 10.9f;
+        s = "Republica Moldova";
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(r);
+        System.out.println(s);
+    }
+}
