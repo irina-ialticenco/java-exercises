56299790363f35dad7108fea469921c4147814ff
Irina Ialticenco <irina.braghina@gmail.com>
2021-08-30 13:32:52 +0300

    extract method

--- MetainitExamples/src/Program.java
+++ MetainitExamples/src/Program.java
@@ -5,43 +5,39 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Добро пожаловать на кто хочет стать миллионером!");
-        System.out.println("Какой цвет получится,если смешать красный и синий?");
-        System.out.println("1-бирюзовый 2-фиолетовый 3-желтый 4-коричневый");
-        Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
+        boolean answer;
 
-        switch (number) {
-            case 2:
-                System.out.println("Правильно");
-                break;
-            default:
-                System.out.println("вы проиграли");
-                return;
+        answer = askQuestion("Какой цвет получится,если смешать красный и синий?: " +
+                "1-бирюзовый 2-фиолетовый 3-желтый 4-коричневый", 2);
+
+        if (!answer)
+            return;
+
+        answer = askQuestion("Какая самая длинная река в мире?:" +
+                "1- Янцзы 2-Нил 3-Амазонка 4- Миссисипи", 3);
+
+        if (!answer)
+            return;
+
+        answer = askQuestion("От чего яблоко падает недалеко?:" +
+                "1- от червяка 2- от садовода 3-от Ньютона 4- от яблони", 4);
+
+        if (answer) {
+            System.out.println("Вы победили!");
         }
+    }
 
-        System.out.println("Какая самая длинная река в мире?:" +
-                "1- Янцзы 2-Нил 3-Амазонка 4- Миссисипи");
-        int number2 = in.nextInt();
-        switch (number2) {
-            case 3:
-                System.out.println("Правильно");
-                break;
-            default:
-                System.out.println("вы проиграли");
-           return;
-        }
-        System.out.println("От чего яблоко падает недалеко?:" +
-                "1- от червяка 2- от садовода 3-от Ньютона 4- от яблони");
-        int number3 = in.nextInt();
+    private static boolean askQuestion(String question, int rightAnswer) {
+        System.out.println(question);
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
-        switch (number3) {
-
-            case 4:
-                System.out.println("Вы-победитель!");
-                break;
-            default:
-                System.out.println("вы проиграли");
-                return;
+        if (number == rightAnswer) {
+            System.out.println("Правильно!");
+            return true;
+        } else {
+            System.out.println("Вы проиграли");
+            return false;
         }
     }
 }
