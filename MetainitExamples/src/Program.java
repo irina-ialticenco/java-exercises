ab77ae12cb51190d3412d11acff91c486efc7dcb
Irina Ialticenco <irina.braghina@gmail.com>
2021-08-30 13:50:21 +0300

    millionaireMap

--- MetainitExamples/src/Program.java
+++ MetainitExamples/src/Program.java
@@ -1,30 +1,29 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         System.out.println("Добро пожаловать на кто хочет стать миллионером!");
-        boolean answer;
 
-        answer = askQuestion("Какой цвет получится,если смешать красный и синий?: " +
+        HashMap<String, Integer> questions = new HashMap<String, Integer>();
+        questions.put("Какой цвет получится,если смешать красный и синий?: " +
                 "1-бирюзовый 2-фиолетовый 3-желтый 4-коричневый", 2);
-
-        if (!answer)
-            return;
-
-        answer = askQuestion("Какая самая длинная река в мире?:" +
+        questions.put("Какая самая длинная река в мире?:" +
                 "1- Янцзы 2-Нил 3-Амазонка 4- Миссисипи", 3);
-
-        if (!answer)
-            return;
-
-        answer = askQuestion("От чего яблоко падает недалеко?:" +
+        questions.put("От чего яблоко падает недалеко?:" +
                 "1- от червяка 2- от садовода 3-от Ньютона 4- от яблони", 4);
 
-        if (answer) {
-            System.out.println("Вы победили!");
+        for (Map.Entry<String, Integer> entry : questions.entrySet()){
+            boolean answer = askQuestion(entry.getKey(), entry.getValue());
+            if (!answer){
+                return;
+            }
         }
+
+        System.out.println("Вы победили!");
     }
 
     private static boolean askQuestion(String question, int rightAnswer) {
