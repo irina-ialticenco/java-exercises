19cb030da2bd07945755180efc366faec89ec0f8
Irina Ialticenco <irina.braghina@gmail.com>
2021-09-06 11:02:36 +0300

    Implement method Scanner

---
+++ Homework 3/src/HomeworkScanner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HomeworkScanner {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Buna ziua!");
+        System.out.println("Introduceti numele Dvs:");
+        String inputNume = scanner.nextLine();
+        System.out.println("Buna ziua, "+ inputNume);
+
+    }
+}
